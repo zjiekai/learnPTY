7500dc79a257969ce0a31e643ab2289e937ac7e1
zjk <jiekai.zheng@transwarp.io>
2016-09-02 15:49:54 +0800

    add react-router

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
+const routes = <Route component={App}>
+  <Route path="/" component={Voting} />
+</Route>;
+
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Router history={hashHistory}>{routes}</Router>,
   document.getElementById('app')
 );
